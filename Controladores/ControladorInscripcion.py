0a6223a2c8d45b4cfd4cedbf813dd139d2b9f5db
felipebuitragocarmona <felipebuitragocarmona@gmail.com>
2022-04-25 10:38:03 -0500

    first commit

---
+++ Controladores/ControladorInscripcion.py
@@ -0,0 +1,8 @@
+from Modelos.Inscripcion import Inscripcion
+from Modelos.Estudiante import Estudiante
+from Modelos.Materia import Materia
+from Repositorios.RepositorioInscripcion import RepositorioInscripcion
+from Repositorios.RepositorioEstudiante import RepositorioEstudiante
+from Repositorios.RepositorioMateria import RepositorioMateria
+class ControladorInscripcion():
+    pass
---
+++ Controladores/ControladorMateria.py
@@ -0,0 +1,24 @@
+from Repositorios.RepositorioMateria import RepositorioMateria
+from Repositorios.RepositorioDepartamento import RepositorioDepartamento
+from Modelos.Materia import Materia
+from Modelos.Departamento import Departamento
+class ControladorMateria():
+    def __init__(self):
+        self.repositorioMateria = RepositorioMateria()
+        self.repositorioDepartamento = RepositorioDepartamento()
+    def index(self):
+        return self.repositorioMateria.findAll()
+    def create(self,infoMateria):
+        nuevoMateria=Materia(infoMateria)
+        return self.repositorioMateria.save(nuevoMateria)
+    def show(self,id):
+        elMateria=Materia(self.repositorioMateria.findById(id))
+        return elMateria.__dict__
+
+    def update(self,id,infoMateria):
+        materiaActual=Materia(self.repositorioMateria.findById(id))
+        materiaActual.nombre=infoMateria["nombre"]
+        materiaActual.creditos = infoMateria["creditos"]
+        return self.repositorioMateria.save(materiaActual)
+    def delete(self,id):
+        return self.repositorioMateria.delete(id)
